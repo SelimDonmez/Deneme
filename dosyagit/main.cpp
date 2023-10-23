9f27e2425bd53468ac2606acfa727351b857f34b
Selim <152120211135@ogrenci.ogu.edu.tr>
2023-10-23 22:32:56 +0300

    Initial Commit

---
+++ dosyagit/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  dosyagit
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    std::cout << "githuba atmaya çalışıyoz";
+
+    return 0;
+}
